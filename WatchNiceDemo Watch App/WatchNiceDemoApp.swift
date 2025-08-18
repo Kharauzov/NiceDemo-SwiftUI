73fab51331ecfcfde680a0669da42d279645fedf
Serhii Kharauzov <serhii.kharauzov@gmail.com>
2025-08-18 19:33:29 +0200

    Add Watch target

---
+++ WatchNiceDemo Watch App/WatchNiceDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchNiceDemoApp.swift
+//  WatchNiceDemo Watch App
+//
+//  Created by Serhii Kharauzov on 18.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct WatchNiceDemo_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
